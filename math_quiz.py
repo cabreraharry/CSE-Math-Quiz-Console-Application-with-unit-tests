abee887a8393b43f0631ffb8af4c066b4e7439c4
Harry Cabrera <harryericsoncabrera15@gmail.com>
2023-12-18 22:56:15 +0800

    User Quess added

--- math_quiz.py
+++ math_quiz.py
@@ -14,10 +14,33 @@ def divide(x, y):
         raise ValueError("Cannot divide by zero")
 
 def generate_quiz_question():
+
     import random
     x = random.randint(1, 10)
     y = random.randint(1, 10)
     operator = random.choice(['+', '-'])
     question = f"{x} {operator} {y}"
-    answer = eval(question)
+    answer = eval(question) 
     return question, answer
+
+def play_quiz():
+    score = 0
+    num_questions = 5  
+    for _ in range(num_questions):
+        question, answer = generate_quiz_question()
+        user_answer = input(f"What is the answer to {question}? ")
+        try:
+            user_answer = float(user_answer)
+            if user_answer == answer:
+                print("Correct!\n")
+                score += 1
+            else:
+                print(f"Wrong! The correct answer is {answer}\n")
+        except ValueError:
+            print("Invalid input. Please enter a number.\n")
+
+    print(f"Quiz completed! Your score is {score}/{num_questions}")
+
+if __name__ == "__main__":
+    play_quiz()
+
